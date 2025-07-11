620f66ce2b40eb1731fe7d11f57bc8e4070756b0
Mike Rivera <rivera.mj@gmail.com>
2025-07-11 19:02:32 +0800

    add common types

---
+++ src/resources/common-types.ts
@@ -0,0 +1,14 @@
+export type NetworkIds =
+    | 1 // Ethereum
+    | 10 // Optimism
+    | 56 // BNB
+    | 137 // Polygon
+    | 8453 // Base
+    | 42161 // Arbitrum
+    | 900 // Solana (Demo environment only)
+    | 11155111 // Sepolia (Demo environment only)
+    | 901; // Solana Devnet (Demo environment only)
+
+export type SortDir = "asc" | "desc";
+
+export type SettlementType = "Crypto" | "Fiat";
