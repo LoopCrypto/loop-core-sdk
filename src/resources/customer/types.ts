173bfe642f9e1a27559ce9a8ff43a1bc1fbcfb47
Mike Rivera <rivera.mj@gmail.com>
2025-07-11 19:06:10 +0800

    customer refactor

--- src/resources/customer/types.ts
+++ src/resources/customer/types.ts
@@ -1,57 +1,67 @@
+import { NetworkIds, SortDir } from "../common-types";
+
+/**
+ * Customer Request
+ */
+
 export interface CustomerQueryParams {
     customerId?: string;
     paymentMethodId?: string;
     merchantId?: string;
     customerRefId?: string;
     subscriptionRefId?: string;
-    page?: number; // Using number instead of double in TypeScript
-    limit?: number; // Using number instead of double in TypeScript
+    page?: number;
+    limit?: number;
     sortBy?: "customerId" | "customerRefId" | "dateCreated";
-    sortDir?: "asc" | "desc";
+    sortDir?: SortDir;
 }
 
-export interface CustomerResponse {
-    totalResults: number; // Using number instead of double in TypeScript
-    customers: CustomerType[];
-}
-
-export interface CustomerType {
-    customerId: string;
-    customerRefId: string | null;
-    subscriptionRefId: string | null;
-    merchant: Merchant;
-    paymentMethods: PaymentMethod[];
-    dateCreated: number;
-}
-
-interface Merchant {
-    merchantId: string;
-    merchantName: string;
-    merchantRefId: string;
-}
-
-interface PaymentMethod {
-    networkId: 1 | 10 | 56 | 137 | 8453 | 42161 | 900 | 11155111 | 901;
-    paymentMethodId: string;
-    paymentMethodName: string;
+export interface CreatePaymentMethodRequest {
+    name: string;
+    networkId: NetworkIds;
     walletAddress: string;
-    isDefault: boolean;
-    token: Token;
-    preAuthorization: PreAuthorization;
+    tokenAddress?: string;
+    tokenSymbol?: string;
+    isDefault?: boolean;
+    authorizationSignature: string;
+}
+export interface CustomerRequestBody {
+    merchantId: string;
+    customerRefId?: string;
+    subscriptionRefId?: string;
+    paymentMethod?: CreatePaymentMethodRequest;
+}
+
+/**
+ * Customer Response
+ */
+
+export interface MerchantCustomerRef {
+    /**
+     * The unique identifier that represents the merchant
+     * @example "67e55044-10b1-426f-9247-bb680e5fe0c8"
+     */
+    merchantId: string;
+    /**
+     * The external customer reference ID used to tie this customer to a customer in an external system.
+     * @example "cus_1234abcd"
+     */
+    customerRefId: string | null;
+}
+
+interface TokenExchangeRateResponse {
+    currency: string;
+    price: string; // Accurate to 4 decimal places
+    provider: string;
+    timestamp: number;
 }
 
 interface Token {
-    symbol: string;
     tokenId: string;
+    symbol: string;
     address: string;
     decimals: number;
-    exchangeRates: ExchangeRate[];
-}
-
-interface ExchangeRate {
-    currency: "USD";
-    price: string; // Accurate to 4 decimal places
-    timestamp: number;
+    exchangeRates: TokenExchangeRateResponse[];
 }
 
 interface PreAuthorization {
@@ -59,19 +69,26 @@ interface PreAuthorization {
     authorization: string;
 }
 
-export interface CustomerRequestBody {
+interface PaymentMethodResponse {
+    paymentMethodId: string;
     merchantId: string;
-    customerRefId?: string;
-    subscriptionRefId?: string;
-    paymentMethodId?: string;
-    paymentMethod?: PaymentMethodRequest;
+    paymentMethodName: string;
+    active: boolean;
+    networkId: NetworkIds;
+    walletAddress: string;
+    isDefault: boolean;
+    token: Token;
+    preAuthorization: PreAuthorization;
 }
 
-interface PaymentMethodRequest {
-    networkId: 1 | 10 | 56 | 137 | 8453 | 42161 | 900 | 11155111 | 901;
-    walletAddress: string;
-    name: string;
-    tokenAddress?: string;
-    tokenSymbol?: string;
-    authorizationSignature: string;
+export interface CustomerType {
+    customerId: string;
+    customerRefIds: MerchantCustomerRef[];
+    paymentMethods: PaymentMethodResponse[];
+    dateCreated: number;
+}
+
+export interface CustomerResponse {
+    totalResults: number;
+    customers: CustomerType[];
 }
