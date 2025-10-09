710b21bce9b0214712717a8a50a67cda5d4af5b5
Mike Rivera <mike@loopcrypto.xyz>
2025-10-09 13:56:39 +0300

    Fix: Payin optional Field (#10)
    * optional field
    
    * version bump

--- src/resources/payin/types.ts
+++ src/resources/payin/types.ts
@@ -43,7 +43,7 @@ export interface UpdatePayinRequest {
     status?: "scheduled" | "canceled";
     amount?: string; // Payment amount in cents (for fiat) or including decimal places (for tokens)
     amountType?: AmountType; // Type of the amount ("fiat" or "token")
-    billDate: number; // The date the payment should take place, represented as a Unix timestamp
+    billDate?: number; // The date the payment should take place, represented as a Unix timestamp
     paymentMethodId?: string; // The ID of the customer's payment method to use for this payin (optional, or customerId must be provided)
     externalInvoiceRef?: string; //  An invoice reference ID used to tie this payin to an invoice in an external system.
     description?: string; // A description or note for the payin, up to 500 characters (optional)
@@ -56,10 +56,10 @@ export interface UpdatePayinRequest {
 export interface PayinPaymentMethodResponse
     extends Omit<PaymentMethodResponse, "merchantId" | "dateCreated"> {
     status:
-    | "ok"
-    | "insufficient_balance"
-    | "insufficient_authorization"
-    | "insufficient_balance_authorization";
+        | "ok"
+        | "insufficient_balance"
+        | "insufficient_authorization"
+        | "insufficient_balance_authorization";
 }
 
 export interface PreAuthorization {
@@ -85,13 +85,13 @@ export interface PayinResponse {
     externalInvoiceRef: string | null;
     payinType: "subscription" | "invoice";
     payinStatus:
-    | "scheduled"
-    | "pending"
-    | "completed"
-    | "failed"
-    | "canceled"
-    | "uncollectible"
-    | "draft";
+        | "scheduled"
+        | "pending"
+        | "completed"
+        | "failed"
+        | "canceled"
+        | "uncollectible"
+        | "draft";
     transaction: PayinTransactionResponse | null;
     paymentMethod: PayinPaymentMethodResponse;
     payoutDestination: Omit<
